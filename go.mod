1c92aac44ed896bb2333edbed6e720f6446f1249
nchoquet <nicolachoquet06250@gmail.com>
2025-09-08 11:25:34 +0200

    change sqlite to mysql

--- go.mod
+++ go.mod
@@ -3,10 +3,12 @@ module semigraphical-app
 go 1.25
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/awesome-gocui/gocui v1.1.0 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/gdamore/encoding v1.0.0 // indirect
 	github.com/gdamore/tcell/v2 v2.4.0 // indirect
+	github.com/go-sql-driver/mysql v1.9.3 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.0.3 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"log"
 	"strings"
 
-	"github.com/awesome-gocui/gocui"
+	. "github.com/awesome-gocui/gocui"
 
-	_ "modernc.org/sqlite"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type User struct {
@@ -26,32 +27,25 @@ var (
 	password     string
 )
 
-func layout(g *gocui.Gui) error {
-	maxX, maxY := g.Size()
-
-	g.Cursor = true
-
-	if v, err := g.SetView("login-frame", 1, 1, maxX-1, maxY-1, 0); err != nil {
-		if err != gocui.ErrUnknownView {
+func createLoginButton(g *Gui, maxX, maxY int) error {
+	if v, err := g.SetView("login", maxX/2-10, maxY/2+3, maxX/2+10, maxY/2+5, 0); err != nil {
+		if !errors.Is(err, ErrUnknownView) {
 			return err
 		}
-		v.Frame = true
-		v.Title = "Login"
+		v.Clear()
+		fmt.Fprintln(v, "Login (Press Enter)")
 	}
 
-	// Champ email
-	if v, err := g.SetView("email", maxX/2-20, maxY/2-4, maxX/2+20, maxY/2-2, 0); err != nil {
-		if err != gocui.ErrUnknownView {
-			return err
-		}
-		v.Title = "Email"
-		v.Editable = true
-		v.Wrap = false
+	if err := g.SetKeybinding("login", KeyEnter, ModNone, doLogin); err != nil {
+		return err
 	}
 
-	// Champ mot de passe
+	return nil
+}
+
+func createPasswordField(g *Gui, maxX, maxY int) error {
 	if v, err := g.SetView("password", maxX/2-20, maxY/2-1, maxX/2+20, maxY/2+1, 0); err != nil {
-		if err != gocui.ErrUnknownView {
+		if !errors.Is(err, ErrUnknownView) {
 			return err
 		}
 		v.Title = "Password"
@@ -60,12 +54,48 @@ func layout(g *gocui.Gui) error {
 		v.Mask = '*'
 	}
 
-	// Bouton login
-	if v, err := g.SetView("login", maxX/2-10, maxY/2+3, maxX/2+10, maxY/2+5, 0); err != nil {
-		if err != gocui.ErrUnknownView {
+	return nil
+}
+
+func createEmailField(g *Gui, maxX, maxY int) error {
+	if v, err := g.SetView("email", maxX/2-20, maxY/2-4, maxX/2+20, maxY/2-2, 0); err != nil {
+		if !errors.Is(err, ErrUnknownView) {
 			return err
 		}
-		fmt.Fprint(v, "Login (Press Enter)")
+		v.Title = "Login"
+		v.Editable = true
+		v.Wrap = false
+	}
+
+	return nil
+}
+
+func layout(g *Gui) error {
+	maxX, maxY := g.Size()
+
+	g.Cursor = true
+
+	if v, err := g.SetView("login-frame", 1, 1, maxX-1, maxY-1, 0); err != nil {
+		if !errors.Is(err, ErrUnknownView) {
+			return err
+		}
+		v.Frame = true
+		v.Title = "Login"
+	}
+
+	// Champ email
+	if err := createEmailField(g, maxX, maxY); err != nil {
+		return err
+	}
+
+	// Champ mot de passe
+	if err := createPasswordField(g, maxX, maxY); err != nil {
+		return err
+	}
+
+	// Bouton login
+	if err := createLoginButton(g, maxX, maxY); err != nil {
+		return err
 	}
 
 	if _, err := g.SetCurrentView(widgets[currentIndex]); err != nil {
@@ -76,11 +106,11 @@ func layout(g *gocui.Gui) error {
 	return nil
 }
 
-func layout2(g *gocui.Gui) error {
+func layout2(g *Gui) error {
 	maxX, maxY := g.Size()
 
 	if v, err := g.SetView("main-frame", 1, 1, maxX-1, maxY-1, 0); err != nil {
-		if err != gocui.ErrUnknownView {
+		if err != ErrUnknownView {
 			return err
 		}
 		v.Frame = true
@@ -90,7 +120,7 @@ func layout2(g *gocui.Gui) error {
 	return nil
 }
 
-func updateCursor(g *gocui.Gui) {
+func updateCursor(g *Gui) {
 	name := widgets[currentIndex]
 	if name == "email" || name == "password" {
 		g.Cursor = true
@@ -99,14 +129,14 @@ func updateCursor(g *gocui.Gui) {
 	}
 }
 
-func nextView(g *gocui.Gui, v *gocui.View) error {
+func nextView(g *Gui, v *View) error {
 	currentIndex = (currentIndex + 1) % len(widgets)
 	_, err := g.SetCurrentView(widgets[currentIndex])
 	updateCursor(g)
 	return err
 }
 
-func doLogin(g *gocui.Gui, v *gocui.View) error {
+func doLogin(g *Gui, v *View) error {
 	// Récupérer email et mot de passe
 	if ev, err := g.View("email"); err == nil {
 		email = strings.TrimSpace(ev.Buffer())
@@ -116,38 +146,42 @@ func doLogin(g *gocui.Gui, v *gocui.View) error {
 	}
 
 	// Affichage du résultat
-	g.Update(func(g *gocui.Gui) error {
+	g.Update(func(g *Gui) error {
+		_ = g.DeleteView("result")
 		maxX, maxY := g.Size()
-		db, err := sql.Open("sqlite", "sqlite.db")
+		db, err := sql.Open("mysql", "nchoquet:nchoquet@tcp(127.0.0.1:3306)/go_semigraphical_app")
 		if err != nil {
 			log.Fatal(err)
 		}
 		defer db.Close()
 
 		if v, err := g.SetView("result", maxX/2-20, maxY/2+7, maxX/2+20, maxY/2+9, 0); err != nil {
-			if err != gocui.ErrUnknownView {
+			if err != ErrUnknownView {
 				return err
 			}
-			v.Title = "Result"
 			v.Clear()
+			v.Title = "Result"
 
-			row := db.QueryRow("SELECT id, first_name, last_name, email, password FROM user WHERE email=? AND password=?", email, password)
+			row := db.QueryRow("SELECT id, first_name, last_name, email, password FROM user WHERE email=? AND password=SHA1(?)", email, password)
 			var user = User{}
 			if err = row.Scan(
 				&user.Id, &user.FirstName,
 				&user.LastName, &user.Email,
 				&user.Password,
 			); err != nil {
+				_ = g.DeleteView("login")
+				_ = createLoginButton(g, maxX, maxY)
 				fmt.Fprintf(v, "Wrong password")
 			} else {
 				fmt.Fprintf(v, "FirstName: %s, LastName: %s", user.FirstName, user.LastName)
+				v.Clear()
 				g.SetManagerFunc(layout2)
 
 				if err := keybindings(g); err != nil {
 					log.Panicln(err)
 				}
 
-				if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+				if err := g.MainLoop(); err != nil && err != ErrQuit {
 					log.Panicln(err)
 				}
 			}
@@ -158,28 +192,24 @@ func doLogin(g *gocui.Gui, v *gocui.View) error {
 	return nil
 }
 
-func keybindings(g *gocui.Gui) error {
+func keybindings(g *Gui) error {
 	// Tabulation pour naviguer
-	if err := g.SetKeybinding("", gocui.KeyTab, gocui.ModNone, nextView); err != nil {
-		return err
-	}
-	// Entrée sur le bouton login
-	if err := g.SetKeybinding("login", gocui.KeyEnter, gocui.ModNone, doLogin); err != nil {
+	if err := g.SetKeybinding("", KeyTab, ModNone, nextView); err != nil {
 		return err
 	}
 	// Quitter
-	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+	if err := g.SetKeybinding("", KeyCtrlC, ModNone, quit); err != nil {
 		return err
 	}
 	return nil
 }
 
-func quit(g *gocui.Gui, v *gocui.View) error {
-	return gocui.ErrQuit
+func quit(g *Gui, v *View) error {
+	return ErrQuit
 }
 
 func main() {
-	g, err := gocui.NewGui(gocui.OutputNormal, true)
+	g, err := NewGui(OutputNormal, true)
 	if err != nil {
 		log.Panicln(err)
 	}
@@ -191,7 +221,7 @@ func main() {
 		log.Panicln(err)
 	}
 
-	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+	if err := g.MainLoop(); err != nil && err != ErrQuit {
 		log.Panicln(err)
 	}
 }
